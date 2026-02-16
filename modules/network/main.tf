8089da5a4a66f467148fcddaea237c091b3fe45d
dltnals1210 <dltnalstnals14@gmail.com>
2026-02-16 16:46:50 +0900

    `fix(shared-network): keep interface vpce on one subnet per az while adding prod private subnets`

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -220,7 +220,7 @@ resource "aws_vpc_endpoint" "interface" {
   vpc_id              = var.vpc_id
   service_name        = each.value
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = concat(var.private_subnet_ids, values(aws_subnet.private_prod)[*].id)
+  subnet_ids          = var.private_subnet_ids
   security_group_ids  = [aws_security_group.vpce.id]
   private_dns_enabled = true
 
