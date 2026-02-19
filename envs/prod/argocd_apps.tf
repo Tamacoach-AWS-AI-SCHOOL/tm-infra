27306c873a226ab1b7b5ed5fe2fd72ba3fffd229
dltnals1210 <dltnalstnals14@gmail.com>
2026-02-19 21:46:33 +0900

    fix(argocd): switch dev/prod applications to multi-source helm+values

--- envs/prod/argocd_apps.tf
+++ envs/prod/argocd_apps.tf
@@ -1,9 +1,12 @@
 locals {
-  argocd_prod_app_name        = "apps-${var.env}-${var.project}"
-  argocd_prod_app_project     = "prod"
-  argocd_prod_app_repo_url    = var.argocd_project_source_repos[0]
-  argocd_prod_app_target_rev  = "main"
-  argocd_prod_app_source_path = "env/prod"
+  argocd_prod_app_name            = "apps-${var.env}-${var.project}"
+  argocd_prod_app_project         = "prod"
+  argocd_prod_values_repo_url     = var.argocd_project_source_repos[0]
+  argocd_prod_values_target_rev   = "main"
+  argocd_prod_helm_repo_url       = var.argocd_project_source_repos[2]
+  argocd_prod_helm_target_rev     = "main"
+  argocd_prod_helm_chart_path     = "charts/tm-app"
+  argocd_prod_helm_values_refpath = "$values/env/prod/values.yaml"
 }
 
 resource "kubernetes_manifest" "argocd_application_prod" {
@@ -21,11 +24,21 @@ resource "kubernetes_manifest" "argocd_application_prod" {
     }
     spec = {
       project = local.argocd_prod_app_project
-      source = {
-        repoURL        = local.argocd_prod_app_repo_url
-        targetRevision = local.argocd_prod_app_target_rev
-        path           = local.argocd_prod_app_source_path
-      }
+      sources = [
+        {
+          repoURL        = local.argocd_prod_helm_repo_url
+          targetRevision = local.argocd_prod_helm_target_rev
+          path           = local.argocd_prod_helm_chart_path
+          helm = {
+            valueFiles = [local.argocd_prod_helm_values_refpath]
+          }
+        },
+        {
+          repoURL        = local.argocd_prod_values_repo_url
+          targetRevision = local.argocd_prod_values_target_rev
+          ref            = "values"
+        },
+      ]
       destination = {
         server    = "https://kubernetes.default.svc"
         namespace = var.apps_namespace
