60282efb828efaf5e9c5e34c2143d8da98bb678d
dltnals1210 <dltnalstnals14@gmail.com>
2026-02-20 11:44:46 +0900

    refactor(argocd,cloudfront): bootstrap from manifest repo and route /api to apigw origins

--- envs/prod/argocd_apps.tf
+++ envs/prod/argocd_apps.tf
@@ -1,12 +1,9 @@
 locals {
-  argocd_prod_app_name            = "apps-${var.env}-${var.project}"
+  argocd_prod_app_name            = "root-${var.env}-${var.project}"
   argocd_prod_app_project         = "prod"
-  argocd_prod_values_repo_url     = var.argocd_project_source_repos[0]
-  argocd_prod_values_target_rev   = "main"
-  argocd_prod_helm_repo_url       = var.argocd_project_source_repos[2]
-  argocd_prod_helm_target_rev     = "main"
-  argocd_prod_helm_chart_path     = "charts/tm-app"
-  argocd_prod_helm_values_refpath = "$values/env/prod/values.yaml"
+  argocd_prod_manifest_repo_url   = var.argocd_project_source_repos[0]
+  argocd_prod_manifest_target_rev = "main"
+  argocd_prod_manifest_path       = "env/prod"
 }
 
 resource "kubernetes_manifest" "argocd_application_prod" {
@@ -24,24 +21,20 @@ resource "kubernetes_manifest" "argocd_application_prod" {
     }
     spec = {
       project = local.argocd_prod_app_project
-      sources = [
-        {
-          repoURL        = local.argocd_prod_helm_repo_url
-          targetRevision = local.argocd_prod_helm_target_rev
-          path           = local.argocd_prod_helm_chart_path
-          helm = {
-            valueFiles = [local.argocd_prod_helm_values_refpath]
-          }
-        },
-        {
-          repoURL        = local.argocd_prod_values_repo_url
-          targetRevision = local.argocd_prod_values_target_rev
-          ref            = "values"
-        },
-      ]
+      source = {
+        repoURL        = local.argocd_prod_manifest_repo_url
+        targetRevision = local.argocd_prod_manifest_target_rev
+        path           = local.argocd_prod_manifest_path
+      }
       destination = {
         server    = "https://kubernetes.default.svc"
-        namespace = var.apps_namespace
+        namespace = var.argocd_namespace
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
       }
     }
   }
